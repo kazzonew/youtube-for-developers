8dbf75ae440d90a96bc271afcb4d2aaa4c313605
kazzonew <81191063+kazzonew@users.noreply.github.com>
2021-07-07 12:33:32 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,70 +9,57 @@ import WhatYoullLearn from '../components/WhatYoullLearn';
 import YouTubeForMe from '../components/YouTubeForMe';
 export default function Home() {
     return (
-        <div>
-            <Head>
-                <title>YouTube For Developers</title>
-                <link rel="icon" href="/youtube.png" />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&family=Roboto:ital,wght@0,100;1,100&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
+         <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome to our portal!</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900" rel="stylesheet">
+    <link rel="stylesheet" href="https://021done.github.io/css/scribbler-global.css">
+    <link rel="stylesheet" href="https://021done.github.io/css/scribbler-doc.css">
+    <link rel="canonical" href="/" />
+  </head>
+  <body>
+    <div class="doc__bg"></div>
+    <nav class="header">
+      <h1 class="logo">FlowingJet.wealthyfast.club</h1>
+      <ul class="menu">
+        <div class="menu__item toggle"><span></span></div>
+        <li class="menu__item"><a href="https://increasedadrtf.com" class="link link--dark"><i class="fa fa-github"></i> Github</a></li>
+        <li class="menu__item"><a href="" class="link link--dark"><i class="fa fa-home"></i> Home</a></li>
+      </ul>
+    </nav>
+    <div class="wrapper">
+      <aside class="doc__nav">
+        <ul>
+          <li class="js-btn selected"><a class="twitter-timeline" data-width="220" data-height="200" href="https://twitter.com/moz?ref_src=twsrc%5Etfw" rel="ugc,nofollow">Tweets by guardian</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script></li>
+          <li class="js-btn selected">
+          </li>
+        </ul>
+      </aside>
+      <article class="doc__content">
+        <section class="js-section">
+          <h3 class="section__title">Latest Google News</h3>
+          <a class="twitter-timeline" data-width="750" data-height="350" data-theme="white" href="https://twitter.com/Google?ref_src=twsrc%5Etfw" rel="ugc,nofollow">Tweets by Google</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        </section>
+        <section class="js-section">
+          <h1 class="section__title">What is SEO good for?</h1>
+          <p>Webmasters and content providers began optimizing websites for search engines in the mid-1990s, as the first search engines were cataloging the early Web. Initially, all webmasters only needed to submit the address of a page, or URL, to the various engines which would send a web crawler to crawl that page, extract links to other pages from it, and return information found on the page to be indexed.[4] The process involves a search engine spider downloading a page and storing it on the search engine's own server. A second program, known as an indexer, extracts information about the page, such as the words it contains, where they are located, and any weight for specific words, as well as all links the page contains. All of this information is then placed into a scheduler for crawling at a later date. Website owners recognized the value of a high ranking and visibility in search engine results,[5] creating an opportunity for both white hat and black hat SEO practitioners. According to industry analmmyst Danny Sullivan, the phrase "search engine optimization" probably came into use in 1997. </p>
+          <p>Sullivan credits Bruce Clay as one of the first people to popularize the term.[6] Early versions of search algorithms relied on webmaster-provided information such as the keyword meta tag or index files in engines like ALIWEB. Meta tags provide a guide to each page's content. Using metadata to index pages was found to be less than reliable, however, because the webmaster's choice of keywords in the meta tag could potentially be an inaccurate representation of the site's actual content. Inaccurate, incomplete, and inconsistent data in meta tags could and did cause pages to rank for irrelevant searches.[7][dubious – discuss] Web content providers also manipulated some attributes within the HTML source of a page in an attempt to rank well in search engines.</p>
+          <p>By 1997, search engine designers recognized that webmasters were making efforts to rank well in their search engine, and thatmm some webmasters were even manipulating their rankings in search results by stuffing pages with excessive or irrelevant keywords. </p>
+          Source: <a href="https://en.wikipedia.org/wiki/Search_engine_optimization" rel="ugc,nofollow">Wikipedia</a> 
+           Partner: <a href="https://247-nieuws.nl">https://247-nieuws.nl</a>
+          <hr />
+        </section>
+      </article>
+    </div>
 
-            <main className="">
-                <section className="container mx-auto max-w-6xl py-20 px-6 md:px-20">
-                    <Hero title="Developer's Guide to Starting a YouTube Channel" />
-                </section>
-                <section className="text-center p-10">
-                    <a
-                        href="https://www.producthunt.com/posts/youtube-for-developers-ebook?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-youtube-for-developers-ebook"
-                        target="_blank"
-                    >
-                        <img
-                            src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=269994&theme=light"
-                            alt="YouTube For Developers Ebook - Create content that will take your career to the next level! | Product Hunt"
-                            style={{ width: '250px', height: '54px' }}
-                            className="mx-auto"
-                        />
-                    </a>
-                </section>
-                <section className="bg-gray-200">
-                    <ResumeMissingSomething />
-                </section>
+    <footer class="footer">
 
-                <WhatYoullLearn />
-
-                <section className="container mx-auto max-w-6xl py-20 px-6 md:px-20 ">
-                    <YouTubeForMe />
-                </section>
-                <section className="bg-brand-800">
-                    <div className="container mx-auto max-w-6xl py-20 px-4 md:px-20">
-                        <p className="text-4xl text-center text-brand-200 mb-0">
-                            Want to break out of tutorial hell...?
-                        </p>
-                        <p className="text-4xl md:text-6xl font-bold text-center text-brand-100">
-                            BUILD THE TUTORIAL YOURSELF!
-                        </p>
-                    </div>
-                </section>
-
-                <Testimonials />
-
-                <FAQs />
-
-                <section className="container mx-auto max-w-6xl py-20 px-6 md:px-20">
-                    <Hero />
-                </section>
-
-                <section className="bg-gray-200">
-                    <div className="container mx-auto max-w-6xl py-20 px-6 md:px-20">
-                        <Instructor />
-                    </div>
-                </section>
-                {/* <section className="container mx-auto max-w-6xl py-20 px-6 md:px-20">
-                    <Questions />
-                </section> */}
-            </main>
-        </div>
-    );
+    </footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+    <script>hljs.initHighlightingOnLoad();</script>
+    <script src="https://021done.github.io/css/scribbler.js"></script>
+  </body>
 }
